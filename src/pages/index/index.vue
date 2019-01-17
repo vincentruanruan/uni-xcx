7a6a3e9f1fe4d70723631f603a89bfa0a4f766b8
vincentruan <ruan81@vip.qq.com>
2019-01-17 17:32:02 +0800

    保存

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,40 +1,40 @@
 <template>
-	<view class="content">
+  <view class="content">
 
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
-	</view>
+    <view>
+      <text class="title">{{title}}
+
+        <text class="asd">asdasdsa</text>
+      </text>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "Hello"
+    };
+  },
+  onLoad() {},
+  methods: {}
+};
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+<style >
+/* lang="scss" scoped="" type="text/css" */
+.content {
+  text-align: center;
+  height: 400upx;
+}
+.logo {
+  height: 200upx;
+  width: 200upx;
+  margin-top: 200upx;
+}
+.title {
+  font-size: 36upx;
+  color: #8f8f94;
+}
 </style>
